b6e56759605de7970b20d26829058dfa6f72e1b0
lokendra-chaulagain <lokendrachaulagain803@gmail.com>
2022-10-25 08:30:31 +0545

    folder structure changed and wallet connection successful with frontend

---
+++ backend/hardhat.config.js
@@ -0,0 +1,17 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+module.exports = {
+  solidity: "0.8.9",
+  // hardhat: {
+  //   chainId: 31337,
+  // },
+
+  // networks: {
+  //   goerli: {
+  //     url: process.env.ALCHEMY_GOERLI_API_KEY,
+  //     accounts: [process.env.WALLET_PRIVATE_KEY],
+  //   },
+  // },
+};
+
