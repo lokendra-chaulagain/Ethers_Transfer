d9d13b4ad0e8c98f46a096b65ee14ccc03ffc65e
lokendra-chaulagain <lokendrachaulagain803@gmail.com>
2022-10-25 11:40:35 +0545

    playing ariubd state login

--- client/src/components/MainCard.tsx
+++ client/src/components/MainCard.tsx
@@ -4,15 +4,23 @@ import { SiEthereum } from "react-icons/si";
 
 const MainCard = () => {
   const [currentWalletAddress, setCurrentWalletAddress] = useState("");
+  const [connectionStarted, setConnectionStarted] = useState(false);
+  const [connectionPending, setConnectionPending] = useState(false);
+  const [connectionSuccess, setConnectionSuccess] = useState(false);
 
   //first checking window.ethereum object is present or not that means metamask is installed or not
   const connectWallet = async () => {
+    setConnectionStarted(true);
+    setConnectionPending(true);
     if (typeof window != "undefined" && typeof window.ethereum != "undefined") {
       try {
         const accounts = await window.ethereum.request({ method: "eth_requestAccounts" });
         setCurrentWalletAddress(accounts[0]);
+        setConnectionSuccess(true);
+        setConnectionPending(false);
         console.log(`Wallet connected successfully and wallet address is ${accounts[0]}`);
       } catch (error) {
+        setConnectionPending(true);
         console.log(error);
       }
     } else {
@@ -28,10 +36,15 @@ const MainCard = () => {
           const accounts = await window.ethereum.request({ method: "eth_accounts" }); //Note this eth_accounts
           if (accounts.length > 0) {
             setCurrentWalletAddress(accounts[0]);
+            setConnectionSuccess(true);
+            setConnectionPending(false);
           } else {
-            console.log("Wallet is not connected play try again once ");
+            // setConnectionPending(true);
+            console.log("Wallet is not connected please try again once ");
           }
-        } catch (error) {}
+        } catch (error) {
+          // setConnectionPending(true);
+        }
       } else {
         console.log("Please install metamask");
       }
@@ -45,9 +58,13 @@ const MainCard = () => {
       if (typeof window != "undefined" && typeof window.ethereum != "undefined") {
         window.ethereum.on("accountsChanged", (accounts: any) => {
           setCurrentWalletAddress(accounts[0]);
+          setConnectionSuccess(true);
+          setConnectionPending(false);
         });
       } else {
         setCurrentWalletAddress("");
+        setConnectionSuccess(false);
+        setConnectionPending(true);
         console.log("Please install metamask");
       }
     };
@@ -56,10 +73,21 @@ const MainCard = () => {
 
   return (
     <div className="main_card p-3  rounded-1  col-6">
+      {connectionPending && !connectionSuccess  && connectionPending? (
+        <p className="custom_warning_toast alert alert-warning text-center border-0 rounded-1" role="alert">
+          Please accept the Metamask connection request from your wallet !!!
+        </p>
+      ) : null}
       <div className="d-flex justify-content-center mb-4">
-        <button onClick={connectWallet} className="btn custom_button py-2 rounded-pill">
-          {currentWalletAddress ? "Connected" : "Connect wallet"}
-        </button>
+        {connectionSuccess && currentWalletAddress && connectionPending ? (
+          <button disabled onClick={connectWallet} className="btn connection_success_button py-2 rounded-pill">
+            {currentWalletAddress ? "Connected" : "Connect wallet"}
+          </button>
+        ) : (
+          <button onClick={connectWallet} className="btn custom_button py-2 rounded-pill">
+            {currentWalletAddress ? "Connected" : "Connect wallet"}
+          </button>
+        )}
       </div>
 
       <div className="sub_card_bg row d-flex flex-column m-0 rounded-1 ">
@@ -69,7 +97,7 @@ const MainCard = () => {
         </div>
 
         <div className="col d-flex justify-content-between">
-          <p> {currentWalletAddress ? currentWalletAddress : "No any wallet connected !"}  </p>
+          <p> {currentWalletAddress ? currentWalletAddress : "No any wallet connected !"} </p>
           <p>dkkd</p>
         </div>
       </div>
